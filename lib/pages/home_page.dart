dfc942b01f7171573f1a53354923e9644adb425e
”Ayush <ayush.muley1907@outlook.com>
2024-10-16 17:05:32 +0000

    home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,22 +1,24 @@
-import 'package:bmi/bloc/bmi/bmi_bloc.dart';
-import 'package:bmi/widgets/bmi_calculator.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => BmiBloc(),
-      child: Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
-          backgroundColor: Colors.transparent,
-          title: const Text("BMI Calc"),
-        ),
-        body: BmiCalculator(),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        title: const Text('BMI Calc'),
+      ),
+      body: Container(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add),
       ),
     );
   }
